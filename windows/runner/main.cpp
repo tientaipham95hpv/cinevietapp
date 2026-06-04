a555b9dc727aee11467f0acba9da702cfecaa45d
tai14895hpv <cerecerpennie@gmail.com>
2026-06-04 16:29:31 +0000

    Register Windows OAuth callback protocol

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,9 +1,52 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
+#include <string>
 
 #include "flutter_window.h"
 #include "utils.h"
+
+namespace {
+
+void RegisterCineVietUrlProtocol() {
+  wchar_t exe_path[MAX_PATH] = {0};
+  if (::GetModuleFileNameW(nullptr, exe_path, MAX_PATH) == 0) {
+    return;
+  }
+
+  const std::wstring key_path = L"Software\\Classes\\cineviet";
+  HKEY key = nullptr;
+  if (::RegCreateKeyExW(HKEY_CURRENT_USER, key_path.c_str(), 0, nullptr, 0,
+                        KEY_SET_VALUE | KEY_CREATE_SUB_KEY, nullptr, &key,
+                        nullptr) != ERROR_SUCCESS) {
+    return;
+  }
+
+  const wchar_t description[] = L"URL:CineViet Protocol";
+  ::RegSetValueExW(key, nullptr, 0, REG_SZ,
+                   reinterpret_cast<const BYTE*>(description),
+                   sizeof(description));
+  const wchar_t empty[] = L"";
+  ::RegSetValueExW(key, L"URL Protocol", 0, REG_SZ,
+                   reinterpret_cast<const BYTE*>(empty), sizeof(empty));
+  ::RegCloseKey(key);
+
+  HKEY command_key = nullptr;
+  if (::RegCreateKeyExW(
+          HKEY_CURRENT_USER,
+          L"Software\\Classes\\cineviet\\shell\\open\\command", 0, nullptr,
+          0, KEY_SET_VALUE, nullptr, &command_key,
+          nullptr) != ERROR_SUCCESS) {
+    return;
+  }
+  const std::wstring command = L"\"" + std::wstring(exe_path) + L"\" \"%1\"";
+  ::RegSetValueExW(command_key, nullptr, 0, REG_SZ,
+                   reinterpret_cast<const BYTE*>(command.c_str()),
+                   static_cast<DWORD>((command.size() + 1) * sizeof(wchar_t)));
+  ::RegCloseKey(command_key);
+}
+
+}  // namespace
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
@@ -16,6 +59,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   // Initialize COM, so that it is available for use in the library and/or
   // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+  RegisterCineVietUrlProtocol();
 
   flutter::DartProject project(L"data");
 
